ea03d7207fc199934cc86319834bf01641fd049e
Christian Rodriguez <christianarg@outlook.com>
2025-10-03 21:06:30 +0200

    welp it works

--- AgentFrameworkResearch/AgentFrameworkResearch/Program.cs
+++ AgentFrameworkResearch/AgentFrameworkResearch/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GeminiDotnet;
+using GeminiDotnet.Extensions.AI;
+using Microsoft.Extensions.AI;
+
+var options = new GeminiClientOptions { ApiKey = Environment.GetEnvironmentVariable("googleAiStudioApiKey")!, ModelId = "gemini-2.5-flash" };
+IChatClient client =
+    new GeminiChatClient(options);
+
+Console.WriteLine(await client.GetResponseAsync("What is AI?"));
 Console.ReadLine();
