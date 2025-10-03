5bc0ec3b735dd947aae116707e379b97d81899c5
Christian Rodriguez <christianarg@outlook.com>
2025-10-03 21:30:45 +0200

    with gemini for know I have to implement lot of stuff. Gotta dig more into it

--- AgentFrameworkResearch/AgentFrameworkResearch/GeminiAgent.cs
+++ AgentFrameworkResearch/AgentFrameworkResearch/GeminiAgent.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 using Microsoft.Agents.AI;
 using Microsoft.Extensions.AI;
 
-public class GeminiAIAgent(IChatClient chatClient) : AIAgent
+public class GeminiAIAgent(IChatClient chatClient,ChatOptions chatOptions) : AIAgent
 {
     public override AgentThread DeserializeThread(JsonElement serializedThread, JsonSerializerOptions? jsonSerializerOptions = null)
     {
@@ -16,7 +16,7 @@ public class GeminiAIAgent(IChatClient chatClient) : AIAgent
 
     public override async Task<AgentRunResponse> RunAsync(IEnumerable<ChatMessage> messages, AgentThread? thread = null, AgentRunOptions? options = null, CancellationToken cancellationToken = default)
     {
-        var response = await chatClient.GetResponseAsync(messages, cancellationToken: cancellationToken);
+        var response = await chatClient.GetResponseAsync(messages, chatOptions, cancellationToken: cancellationToken);
         return new AgentRunResponse(response);
     }
 
--- AgentFrameworkResearch/AgentFrameworkResearch/Program.cs
+++ AgentFrameworkResearch/AgentFrameworkResearch/Program.cs
@@ -3,9 +3,14 @@ using GeminiDotnet;
 using GeminiDotnet.Extensions.AI;
 using Microsoft.Extensions.AI;
 using Microsoft.Agents.AI;
+using System.ComponentModel;
+
+[Description("Get the current time")]
+DateTimeOffset GetCurrentTime() => DateTimeOffset.UtcNow;
 
 var options = new GeminiClientOptions { ApiKey = Environment.GetEnvironmentVariable("googleAiStudioApiKey")!, ModelId = "gemini-2.5-flash" };
 IChatClient client = new GeminiChatClient(options);
-GeminiAIAgent agent = new GeminiAIAgent(client);
-Console.WriteLine(await agent.RunAsync("what is AI?. Be very concise."));
+var chatOptions = new ChatOptions { Tools = [AIFunctionFactory.Create(GetCurrentTime)] };
+GeminiAIAgent agent = new GeminiAIAgent(client, chatOptions);
+Console.WriteLine(await agent.RunAsync("What time is it?"));
 Console.ReadLine();
